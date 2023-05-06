7b073eb5a4fda7480fe967895893fcbb2cc41253
zhuyuanmin <13058039407@163.com>
2023-05-06 16:53:25 +0800

    Update index.jsx

--- src/pages/demo/index.jsx
+++ src/pages/demo/index.jsx
@@ -1,16 +1,19 @@
-import { Button, Rate } from 'react-vant';
+import { Button } from 'react-vant';
 import { useCounterState } from './createState';
 import counterStore from '@/store/counter'
 
 export default function Demo() {
-  const { counter, double, increaseFn } = useCounterState({ value: 10 });
+  const { counter, double, operateFn } = useCounterState({ value: 10 });
+
+  const tripClick = () => {
+    operateFn(count => count * 3)
+  }
 
   return (
     <div>
       <div>{counter} x 2 = {double}</div>
-      <button onClick={increaseFn}>Increase</button>
-      <Button type='primary'>Primary</Button>
-      <Rate value={3} />
+      <button onClick={() => operateFn(v => v + 1)}>Increase</button>
+      <Button type='primary' onClick={tripClick}>Primary</Button>
 
       <div>状态管理器测试</div>
       <div>{counterStore.count} {counterStore.text}</div>
