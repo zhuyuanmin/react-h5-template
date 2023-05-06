d3be6f3911493089d15e6586e99f54d2e22d7239
zhuyuanmin <13058039407@163.com>
2023-05-06 16:53:45 +0800

    Update createState.js

--- src/pages/demo/createState.js
+++ src/pages/demo/createState.js
@@ -1,10 +1,14 @@
-import { createSetup, ref, computed, onUnmounted } from 'reactivue'
+import { createSetup, ref, computed, onUnmounted, watch } from 'reactivue'
 
 export const useCounterState = createSetup(props => {
   const counter = ref(props.value)
   const double = computed(() => counter.value * 2)
-  const increaseFn = () => counter.value += 1
+  const operateFn = cb => counter.value = cb(counter.value)
+
+  watch(counter, (newVal, oldVal) => {
+    console.log(newVal, oldVal)
+  })
 
   onUnmounted(() => console.log('Goodbye World'))
-  return { counter, double, increaseFn }
+  return { counter, double, operateFn }
 })
